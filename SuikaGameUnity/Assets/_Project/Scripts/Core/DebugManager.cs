9677000bf6fed38aca8c013c350e4c6235cf8f1b
Anton Kuzan <antonkuzan@gmail.com>
2026-04-06 11:08:26 +0300

    Completed debugger and flower collision fix

--- SuikaGameUnity/Assets/_Project/Scripts/Core/DebugManager.cs
+++ SuikaGameUnity/Assets/_Project/Scripts/Core/DebugManager.cs
@@ -4,12 +4,32 @@ using UnityEngine.InputSystem;
 public class DebugManager : MonoBehaviour
 {
     public GameObject debugPanel;
+    public Spawner spawner;
     
-    void Update()
+    public void Update()
     {
         if (Keyboard.current.backquoteKey.wasPressedThisFrame)
         {
             debugPanel.SetActive(!debugPanel.activeSelf);
+        }
+    }
+
+    public void SpawnFlowers10()
+    {
+        int flowersToSpawn = 10;
+
+        for (int i = 0; i < flowersToSpawn-1; i++)
+        {
+            spawner.SpawnFlower();
+        }
+    }
+    
+    public void DeleteFlowers()
+    {
+        GameObject[] flowers = GameObject.FindGameObjectsWithTag("Flower");
+        foreach (GameObject flower in flowers)
+        {
+            Destroy(flower);
         }
     }
 }
