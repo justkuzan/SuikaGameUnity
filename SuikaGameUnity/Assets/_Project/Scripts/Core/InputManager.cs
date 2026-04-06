84e6fca0a688f3b7eab89274304b64e4081440ac
Anton Kuzan <antonkuzan@gmail.com>
2026-04-06 17:29:19 +0300

    Fixed input system and Canvas scale on several canvases

--- SuikaGameUnity/Assets/_Project/Scripts/Core/InputManager.cs
+++ SuikaGameUnity/Assets/_Project/Scripts/Core/InputManager.cs
@@ -6,17 +6,28 @@ public class InputManager : MonoBehaviour
     [SerializeField] private Camera mainCamera;
     [SerializeField] private GameSettings settings;
 
+    private InputSystem_Actions _inputActions;
     public float generatedX;
+    
+    private void Awake()
+    {
+        _inputActions = new InputSystem_Actions();
+    }
+    
+    private void OnEnable() => _inputActions.Enable();
+    private void OnDisable() => _inputActions.Disable();
     
     void Update()
     {
-        Vector2 screenPos = Mouse.current.position.ReadValue();
+        Vector2 screenPos = _inputActions.UI.Point.ReadValue<Vector2>();
+        
         Vector3 mouseWorldPosition = new Vector3(screenPos.x, screenPos.y, Mathf.Abs(mainCamera.transform.position.z));
         Vector3 worldPos = mainCamera.ScreenToWorldPoint(mouseWorldPosition);
+        
         float xPosClamp = Mathf.Clamp(worldPos.x, -settings.movementLimitX, settings.movementLimitX);
         generatedX = xPosClamp;
         
-        if (Mouse.current.leftButton.wasPressedThisFrame)
+        if (_inputActions.Player.Attack.WasPressedThisFrame())
         {
             InputClicked();
         }
--- SuikaGameUnity/Assets/_Project/Scripts/Utils/GameEvents.cs
+++ SuikaGameUnity/Assets/_Project/Scripts/Utils/GameEvents.cs
@@ -4,5 +4,5 @@ public static class GameEvents
 {
     public static Action<bool> OnZoneStatusChanged;
     public static Action OnInputClick;
-    
+    public static Action OnFlowersCollided;
 }
