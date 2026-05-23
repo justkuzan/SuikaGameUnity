f14fe86a3daca54f84231f791abb9afab73e32d3
Anton Kuzan <antonkuzan@gmail.com>
2026-05-23 12:04:39 +0300

    Added UI button on main screen

---
+++ SuikaGameUnity/Assets/_Project/Scripts/UI/CustomToggle.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class CustomToggle : MonoBehaviour
+{
+    [Header("Toggle Image Component")]
+    [SerializeField] private Image imageComponent;
+    
+    [Header("Icons")]
+    [SerializeField] private Sprite iconOn;
+    [SerializeField] private Sprite iconOff;
+    
+    [Header("Settings")]
+    [SerializeField] private bool isOn = true;
+    
+    public UnityEvent<bool> OnToggleChanged;
+    
+    void Start()
+    {
+        UpdateVisual();
+    }
+
+    public void Toggle()
+    {
+        isOn = !isOn;
+        UpdateVisual();
+        
+        OnToggleChanged?.Invoke(isOn);
+    }
+
+    private void UpdateVisual()
+    {
+        if (imageComponent == null) return;
+        
+        imageComponent.sprite = isOn ? iconOn : iconOff;
+    }
+}
